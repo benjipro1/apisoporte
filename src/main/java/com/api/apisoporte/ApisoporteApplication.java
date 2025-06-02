1ccc7fef4f50da163a24bca5df64646a5c6c5ecc
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:11:22 -0400

    Base API Soporte

---
+++ src/main/java/com/api/apisoporte/ApisoporteApplication.java
@@ -0,0 +1,13 @@
+package com.api.apisoporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisoporteApplication.class, args);
+	}
+
+}
